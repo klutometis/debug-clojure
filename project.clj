bea92daec651f7a7f2d42e406ad27356c91e336b
Peter Danenberg <pcd@roxygen.org>
2011-11-28 09:43:07 -0800

    Initial pass

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject debug "1.0.0-SNAPSHOT"
+  :description "debug macro that pprints expressions and values"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ src/debug/core.clj
@@ -0,0 +1,17 @@
+(ns debug.core
+  (:use [clojure.pprint :only (pprint)]))
+
+(defmacro debug-map
+  "Map quoted expressions to their values."
+  [& exprs]
+  `(zipmap (reverse (list ~@(map (fn [expr] `'~expr) exprs)))
+           (reverse (list ~@(map (fn [expr] expr) exprs)))))
+
+(defmacro debug-list
+  "List-associate expressions with their values."
+  [& exprs]
+  `(list ~@(map (fn [expr] `(list '~expr ~expr)) exprs)))
+
+(defmacro debug [& exprs]
+  "Pretty-print expression-value associations."
+  `(pprint (debug-map ~@exprs)))
