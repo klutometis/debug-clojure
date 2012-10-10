60d7f71aab867416267afd4bc5591fd50cb9019d
Peter Danenberg <pcd@roxygen.org>
2012-10-10 01:49:46 -0700

    1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject debug "1.0.1-SNAPSHOT"
+(defproject debug "1.0.1"
   :description "debug macro that pprints expressions and values"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :url "https://github.com/klutometis/debug-clojure")
